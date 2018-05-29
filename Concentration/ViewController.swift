cb384a0757ca6f4ec9577a48feb1dd1c6efd89d2
Mackenzie Kinzel <mkinzel2@kent.edu>
2018-05-28 22:25:00 -0400

    change color of cards, add else case to updateViewFromModel

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -40,7 +40,10 @@ class ViewController: UIViewController {
             let card = game.cards[index]
             if card.isFaceUp {
                 button.setTitle(emoji(for: card), for: UIControlState.normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            } else {
+                button.setTitle("", for: UIControlState.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 0.462745098, green: 0.8392156863, blue: 1, alpha: 1)
             }
         }
     }
